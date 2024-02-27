5aedad30ba06f8349c4e8b580a6a19824e5af8bc
Jacob Quinn <quinn.jacobd@gmail.com>
2024-02-27 15:24:26 -0700

    Check CI is working (#1)
    * Initial commit for basic functionality + wrapping
    
    * Clean up CI

--- src/AwsC.jl
+++ src/AwsC.jl
@@ -1,18 +1,32 @@
 module AwsC
-export hello, domath
 
-"""
-    hello(who::String)
+include("LibAwsC.jl")
+using .LibAwsC
 
-Return "Hello, `who`".
-"""
-hello(who::String) = "Hello, $who"
+export LibAwsC
 
-"""
-    domath(x::Number)
+const SUCCESS = LibAwsC.AWS_OP_SUCCESS
+const ERROR = LibAwsC.AWS_OP_ERR
 
-Return `x + 5`.
-"""
-domath(x::Number) = x + 5
+allocator() = LibAwsC.aws_default_allocator()
+
+mem_acquire(alloc, size) = LibAwsC.aws_mem_acquire(alloc, size)
+mem_release(alloc, ptr) = LibAwsC.aws_mem_release(alloc, ptr)
+
+error_str(err) = LibAwsC.aws_error_str(err)
+
+struct Error <: Exception
+    msg::String
+end
+
+last_error() = Error(LibAwsC.aws_error_str(LibAwsC.aws_last_error()))
+throw_error() = throw(last_error())
+
+byte_buf(n::Integer, ptr::Ptr) = LibAwsC.aws_byte_buf(0, ptr, n, C_NULL)
+
+byte_cursor(n::Integer, ptr::Ptr) = LibAwsC.aws_byte_cursor(n, ptr)
+byte_cursor(s::String) = LibAwsC.aws_byte_cursor_from_c_str(s)
+
+append(to::LibAwsC.aws_byte_buf, from::LibAwsC.aws_byte_cursor) = LibAwsC.aws_byte_buf_append(to, from)
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,18 @@
-using Test, Example
+using Test, AwsC
 
-@test hello("Julia") == "Hello, Julia"
-@test domath(2.0) ≈ 7.0
+@testset "AwsC" begin
+
+alloc = AwsC.allocator()
+@test alloc isa Ptr{LibAwsC.aws_allocator}
+mem = AwsC.mem_acquire(alloc, 10)
+@test mem isa Ptr{Cvoid}
+AwsC.mem_release(alloc, mem)
+
+mem = AwsC.mem_acquire(alloc, 10)
+bb = AwsC.byte_buf(10, mem)
+bc = AwsC.byte_cursor("1234567890")
+AwsC.append(bb, bc)
+@test unsafe_string(Ptr{UInt8}(mem), 10) == "1234567890"
+AwsC.mem_release(alloc, mem)
+
+end
